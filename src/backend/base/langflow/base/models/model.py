2eb4a8aebaf4da1403b6b19357846813fb6c9a22
Nikita Selin <farvard34@gmail.com>
2024-11-06 10:01:52 +0000

    Fix

--- src/backend/base/langflow/base/models/model.py
+++ src/backend/base/langflow/base/models/model.py
@@ -104,19 +104,16 @@ class LCModelComponent(Component):
             # Build a well formatted status message
             content = message.content
             response_metadata = message.response_metadata
-            openai_keys = ["token_usage", "model_name", "finish_reason"]
+            openai_keys = ["usage", "model_name", "finish_reason"]
             inner_openai_keys = ["completion_tokens", "prompt_tokens", "total_tokens"]
             anthropic_keys = ["model", "usage", "stop_reason"]
             inner_anthropic_keys = ["input_tokens", "output_tokens"]
-            if all(key in response_metadata for key in openai_keys) and all(
-                key in response_metadata["token_usage"] for key in inner_openai_keys
-            ):
-                token_usage = response_metadata["token_usage"]
+            if all(key in response_metadata for key in openai_keys):
                 status_message = {
                     "tokens": {
-                        "input": token_usage["prompt_tokens"],
-                        "output": token_usage["completion_tokens"],
-                        "total": token_usage["total_tokens"],
+                        "input": 0,
+                        "output":0,
+                        "total":0,
                         "stop_reason": response_metadata["finish_reason"],
                         "response": content,
                     }
